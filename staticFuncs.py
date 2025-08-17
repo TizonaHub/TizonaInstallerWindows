d34bdd05ee3a218da02535ee5642660c3d935e55
Pablo Martin Morente <politotuto@gmail.com>
2025-08-17 03:54:24 +0200

    updated "DB_PASSWORD" to "DB_USER_PASSWORD"

--- staticFuncs.py
+++ staticFuncs.py
@@ -285,7 +285,7 @@ def createEnv(path,username,password,dbName):
         "ORIGINS":"["+'"*"'+"]",
         "DB_HOST":'127.0.0.1',
         "DB_USER":username,
-        "DB_PASSWORD":password,
+        "DB_USER_PASSWORD":password,
         "DB":dbName,
         "STATIC":'storage',
         "NODE_ENV":"production",
